0aeedfa66b9bdd316bd0ab422ad3f1d589733f1c
fick75 <152058450+fick75@users.noreply.github.com>
2026-02-18 21:09:21 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,10 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.	
+gen-lang-client-0892644925),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.	
+gen-lang-client-0892644925)
       },
       resolve: {
         alias: {
